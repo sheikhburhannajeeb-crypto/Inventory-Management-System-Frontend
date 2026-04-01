ca84309a627cd91e5f15598f8e53e6f1cc88d175
Hassan Ali Abrar <hassanalimughal635@gmail.com>
2026-04-01 16:14:06 +0500

    .

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import axios from 'axios'
 import App from './App.jsx'
 import './index.css'
 import { ThemeProvider } from './context/ThemeContext'
+
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || 'http://localhost:5000'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
